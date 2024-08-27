355640edbb01b6ce77f1127cdd3c2687475c56ce
Nikita Menshov <nikitatop2222@gmail.com>
2024-08-27 16:45:18 +0500

    Change yt link and add time.sleep

--- src/yt.py
+++ src/yt.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-from src.config import YT_LINK 
+
+from src.config import YT_LINK
+
 
 def yt_search(keyword: str):
 
     link = f"{YT_LINK}{keyword}"
 
+    print("Please wait 5 second")
+    time.sleep(5)
     r = requests.get(link)
 
     if r.status_code == 200:
@@ -14,9 +19,10 @@ def yt_search(keyword: str):
 
         get_div = soup.find("div", class_="pure-u-1 pure-u-md-1-4")
         get_href = get_div.find("a")['href']
-        
+
         return f"https://www.youtube.com{get_href}"
 
     else:
         print("Error")
+        return None
         return None
